83c014fe9c92c00c66c79079f5eb3ea3d1478554
luhao <767357266@qq.com>
2019-02-15 18:46:29 +0800

    22

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+
+// lang i18n
+Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false;
 
